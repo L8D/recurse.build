b500b87852bc2c1a5c003e6d6bfaa69fd4f4557c
Tenor Biel <tenorbiel@gmail.com>
2019-03-10 15:05:06 -0400

    add template for authoring new consumer images
    this addresses #3 partially.

---
+++ templates/consumer-image/consumer.js
@@ -0,0 +1,26 @@
+const debug = require('debug')('consumer:<%= name %>')
+
+exports.consumer = async ({
+  fero,
+  db
+}) => {
+  // TODO: replace topic name
+  const foo = await fero('foo', {client: true})
+
+  foo
+    .on('change')
+    .map(({key, value}) => {
+      db[key] = value
+    })
+}
+
+if (require.main === module) {
+  (async () => {
+    const fero = require('fero')
+
+    await exports.worker({
+      fero,
+      db: {}
+    })
+  })()
+}
