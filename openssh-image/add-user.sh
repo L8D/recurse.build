563cccc296ec46de0731725a1c46a8ec4066f355
Tenor Biel <tenorbiel@gmail.com>
2019-03-19 14:06:58 -0400

    use command line arguments to pass public keys instead of stdin
    this is a workaround I have to use after discovering that the `pods/exec`
    resource for the kubernetes API does not support streaming stdin over
    websockets.

--- openssh-image/add-user.sh
+++ openssh-image/add-user.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -ex
 
 USERNAME=$1
+AUTH_KEYS=$2
 
 if ! id -u $USERNAME ; then
   # login is only available with keys, so passwords are not important
@@ -11,7 +12,7 @@ fi
 
 mkdir -p -m 0755 /etc/ssh/authorized-keys
 mkdir -p -m 0600 /home/$USERNAME/.ssh
-tee /etc/ssh/authorized-keys/$USERNAME /home/$USERNAME/.ssh/authorized_keys <&0
+echo $AUTH_KEYS | tee /etc/ssh/authorized-keys/$USERNAME /home/$USERNAME/.ssh/authorized_keys
 chmod 0700 /home/$USERNAME/.ssh/authorized_keys
 chown -R $USERNAME /home/$USERNAME/.ssh
 
